bd35a77d42989ba673c556a2cf04e01c88fa7bcd
John anthony Oling <59799747+Anthony-333@users.noreply.github.com>
2021-12-19 18:00:55 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://myrealm.netlify.app","https://myrealm.at","http://myrealm.at","https://www.myrealm.at","http://www.myrealm.at"],
+    origin: ["http://localhost:3000", "https://myrealm.netlify.app","https://myrealm.at","http://myrealm.at","https://www.myrealm.at","http://www.myrealm.at","https://myrealm.com","http://myrealm.com","https://www.myrealm.com","http://www.myrealm.com"],
     credentials: true,
   })
 );
